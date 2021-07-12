fed98be809ea902f4380e08b978be8fe5e5a708a
hareom284 <zawzawwin@ucsloikaw.edu.mm>
2021-07-12 12:05:18 +0630

    make it better

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 //Config Object 
 let config = {
-    consumer_key: "",
-    consumer_secret: "",
-    access_token: "",
-    access_token_secret: ""
+    consumer_key: "PZapPk54QGSKBZk08keGRNe9n",
+    consumer_secret: "TR8kotihiPwyd2kHRPUJvCM5Nlek9q0zX5MGZtAUyS1A7a3De2",
+    access_token: "1408671728222441473-TXMWlKpXUgdgBL3k5iv7DMxCOUweXs",
+    access_token_secret: "uaOpTrXimDdgWo0v6m6RoYtwWKSqr1F1EgmEKFxN78eNv"
 };
 //Export the Config to access from the main bot app.
 module.exports = config;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ let T = new Twit(require("./config"));
 // search specific hastage and you can change hastage and try another
 let searchHastage = { q: '#WhatsHappeningInMyanmar', count: 1, result_type: 'recent' };
 function retweet() {
-  
+
 
     //this below line is use for searching hastage and save in data variable
     T.get("search/tweets", searchHastage, (err, data) => {
@@ -17,7 +17,7 @@ function retweet() {
         // below if check it is available or not hastage with this
         if (data && data.statuses.length > 0) {
             let tweet = data.statuses[0];
-            
+
             // console.log(tweet);
             //retweet the tweet
 
@@ -25,21 +25,21 @@ function retweet() {
                 // console.log(data);
 
                 console.log("Retweet successful");
-                });
-             //like the tweet 
+            });
+            //like the tweet 
             T.post('favorites/create', { id: tweet.id_str })
 
-              .then(result => {
-                 console.log('Liked tweet successfully!');
-               }).catch(console.error);
-           
-            }
+                .then(result => {
+                    console.log('Liked tweet successfully!');
+                }).catch(console.error);
+
+        }
         else {
             console.log('No tweet on the Hastage', searchHastage.q);
         }
     });
-}   
-    //function recall
-    retweet();
-     // to set time interval for retweet new after every 1000s
-    setInterval(retweet,300 *1000)
+}
+//function recall
+retweet();
+// to set time interval for retweet new after every 1000s
+setInterval(retweet, 300*1000)
