7b74b1ac621524e9c7170dff2ac0094cbd6f0f19
hareom284 <zawzawwin328@gmail.com>
2021-09-13 21:30:15 +0630

    updatead randome time

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ let Twit = require("twit");
 //Create an Instance of Twitter API and Authenticate using App Keys 
 let T = new Twit(require("./config"));
 
+//generate random time 
+var time_taken;
 // search specific hastage and you can change hastage and try another
-let searchHastage = { q: '#WhatsHappeningInMyanmar', count: 1, result_type: 'recent' };
+let searchHastage = { q: 'Myanmar', count: 1, result_type: 'recent' };
 function retweet() {
 
+    time_taken = Math.floor(Math.random()* (100-10)+10);
+
+    console.log(time_taken);
 
     //this below line is use for searching hastage and save in data variable
     T.get("search/tweets", searchHastage, (err, data) => {
@@ -19,20 +24,18 @@ function retweet() {
             let tweet = data.statuses[0];
 
             // console.log(tweet);
-            //retweet the tweet
+            //retweet 
 
             T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
                 // console.log(data);
 
                 console.log("Retweet successful");
             });
-            //like the tweet 
             T.post('favorites/create', { id: tweet.id_str })
 
                 .then(result => {
                     console.log('Liked tweet successfully!');
                 }).catch(console.error);
-
         }
         else {
             console.log('No tweet on the Hastage', searchHastage.q);
@@ -42,4 +45,4 @@ function retweet() {
 //function recall
 retweet();
 // to set time interval for retweet new after every 1000s
-setInterval(retweet, 300*1000)
+setInterval(retweet, time_taken*1000)
